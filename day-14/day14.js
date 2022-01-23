f9628f0b7415deb2d6eca3f6964fde1548a1055a
DoDieu <dodieu710@gmail.com>
2022-01-23 19:24:22 +0900

    sort

--- day-14/day14.js
+++ day-14/day14.js
@@ -3,6 +3,6 @@ function checkL(list){
 }
 function listSort(list){
     list = list.split(",");
-    list.sort(function(a,b){return a-b;});
+    list.sort(function(a,b){return b-a;});
     return list;
 }
