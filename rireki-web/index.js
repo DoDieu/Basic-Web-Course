11ab8e01384f77e8dda00352282ab95f8ed0348f
DoDieu <dodieu710@gmail.com>
2022-02-11 22:49:15 +0900

    rireki web insert

---
+++ rireki-web/index.js
@@ -0,0 +1,8 @@
+function changeInfo() {
+    // var deleteInfo = '<a href="" onclick="deleteInfo()">Huy</a>';
+    // var updateInfo = '<a href="" onclick="updateInfo()"></a>';
+    // var changeInfo = '<a href="" onclick="changeInfo()">Sửa</a>';
+    var inputText = "<input type=\"text\"></>";
+    // //divInfo = divInfo.replace(changeInfo, deleteInfo);
+    document.getElementById("insertInputText").innerHTML = "<input type=\"text\">";
+}
